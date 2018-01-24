acbfde93a4a2724d17a1d92e89104d9e3803de6f
haptikfeedback <32914608+haptikfeedback@users.noreply.github.com>
2018-01-23 21:29:43 -0800

    Update

--- lpthw/languages.py
+++ lpthw/languages.py
@@ -10,12 +10,12 @@ def main(language_file, encoding, errors):
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
 
-    def print_line(line, encoding, errors):
-        next_lang = line.strip()
-        raw_bytes = next_lang.encode(encoding, errors=errors)
-        cooking_strin = raw_bytes.decode(encoding, errors=errors)
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
 
-        print(raw_bytes, "<===>", cooked_string)
+    print(raw_bytes, "<===>", cooked_string)
 
 
 languages = open("languages.txt", encoding="utf-8")
