a1eda51a45fbaf3c75b6e3d74e2a3980a3fe8484
stringking <32914608+haptikfeedback@users.noreply.github.com>
2018-02-05 21:32:46 -0800

    Added unit converter

---
+++ basic/unit_converter.js
@@ -0,0 +1,171 @@
+// var inch = inch * .0254
+// var foot = foot * .3048
+// var yard = yard * .9144
+// var mile = mile * 1609.34
+
+/**
+ * Converts inch to ...
+ */
+function convertInch(to, value){
+    if (to === 'inch'){
+        return value * 1
+    } else if (to === 'feet'){
+        return value * 0.0833333
+    } else if (to === 'yard'){
+        return value * 0.0277778
+    } else if (to === 'mile'){
+        return value * 0.0000157828
+    } else if (to === 'kilometer'){
+        return value * 0.0000254
+    } else if (to === 'meter'){
+        return value * 0.0254
+    } else if (to === 'centimeter'){
+        return value * 2.54
+    } else if (to === 'millimeter'){
+        return value * 25.4
+    }
+}
+/***
+ *  Convert feet to ...
+ */
+function convertFeet(to, value){
+    if (to === 'inch'){
+        return value * 12
+    } else if (to === 'feet'){
+        return value * 1
+    } else if (to === 'yard'){
+        return value * 0.0277778
+    } else if (to === 'mile'){
+        return value * 0.000189394
+    } else if (to === 'kilometer'){
+        return value * 0.000304800097536
+    } else if (to === 'meter'){
+        return Math
+    } else if (to === 'centimeter'){
+        return Math
+    } else if (to === 'millimeter')
+}
+/***
+ *  Convert yard to ...
+ */
+function convertYard(to, value){
+    if (to === 'inch'){
+        return Math
+    } else if (to === 'feet'){
+        return Math
+    } else if (to === 'yard'){
+        return Math
+    } else if (to === 'mile'){
+        return Math
+    } else if (to === 'kilometer'){
+        return Math
+    } else if (to === 'meter'){
+        return Math
+    } else if (to === 'centimeter'){
+        return Math
+    } else if (to === 'millimeter')
+}
+/***
+ * Convert mile to ...
+ */
+function convertMile(to, value){
+    if (to === 'inch'){
+        return Math
+    } else if (to === 'feet'){
+        return Math
+    } else if (to === 'yard'){
+        return Math
+    } else if (to === 'mile'){
+        return Math
+    } else if (to === 'kilometer'){
+        return Math
+    } else if (to === 'meter'){
+        return Math
+    } else if (to === 'centimeter'){
+        return Math
+    } else if (to === 'millimeter')
+}
+/***
+ * Convert Millimeter to ...
+ */
+function convertMillimeter(to, value){
+    if (to === 'inch'){
+        return Math
+    } else if (to === 'feet'){
+        return Math
+    } else if (to === 'yard'){
+        return Math
+    } else if (to === 'mile'){
+        return Math
+    } else if (to === 'kilometer'){
+        return Math
+    } else if (to === 'meter'){
+        return Math
+    } else if (to === 'centimeter'){
+        return Math
+    } else if (to === 'millimeter')
+}
+/***
+ *  Convert Centimeter to ...
+ */
+function convertCentimeter(to, value){
+    if (to === 'inch'){
+        return Math
+    } else if (to === 'feet'){
+        return Math
+    } else if (to === 'yard'){
+        return Math
+    } else if (to === 'mile'){
+        return Math
+    } else if (to === 'kilometer'){
+        return Math
+    } else if (to === 'meter'){
+        return Math
+    } else if (to === 'centimeter'){
+        return Math
+    } else if (to === 'millimeter')
+}
+/***
+ *  Convert Meter to ...
+ */
+function convertMillimeter(to, value){
+    if (to === 'inch'){
+        return Math
+    } else if (to === 'feet'){
+        return Math
+    } else if (to === 'yard'){
+        return Math
+    } else if (to === 'mile'){
+        return Math
+    } else if (to === 'kilometer'){
+        return Math
+    } else if (to === 'meter'){
+        return Math
+    } else if (to === 'centimeter'){
+        return Math
+    } else if (to === 'millimeter')
+}
+/**
+ *  Convert Kilometer to ...
+ */
+function convertKilometer(to, value){
+    if (to === 'inch'){
+        return Math
+    } else if (to === 'feet'){
+        return Math
+    } else if (to === 'yard'){
+        return Math
+    } else if (to === 'mile'){
+        return Math
+    } else if (to === 'kilometer'){
+        return Math
+    } else if (to === 'meter'){
+        return Math
+    } else if (to === 'centimeter'){
+        return Math
+    } else if (to === 'millimeter')
+}
+/**
+ * Generic converter
+ */
+
