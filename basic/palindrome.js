15d6e9c9f7ce71a744bf88ca2cdbad4cd8a5d7c7
stringking <32914608+haptikfeedback@users.noreply.github.com>
2018-02-07 19:21:34 -0800

    Added palindrome detector, Javascript version

--- basic/palindrome.js
+++ basic/palindrome.js
@@ -2,9 +2,30 @@
  * 
  */
 function palindrome(phrase){
-    var cleaned = phrase.replace(/\s/g, "");
-    var reversed = cleaned.reverse();
-    return (cleaned + " " + reversed)
+        //This takes the phrase and removes any spaces
+        var cleaned = phrase.replace(/\s/g, "");
+        console.log(cleaned, " This displays the cleaned phrase.")
+        //This takes the phrase and makes a reversed array
+        var splitString = cleaned.split("").reverse();
+        console.log(splitString)
+        //This takes the phrase and compares it to itself
+        return splitString.join("") == cleaned
 }
 
-console.log(palindrome('te s t'))
+console.log(palindrome('solos'))
+/**
+ * Instructor example of utilizing a for loop to solve the problem
+ * with an output to demonstrate what's happening.
+ */
+// function isPalindrome(phrase) {
+//     var string = phrase.replace(/\s/g, '')
+//     for(var i = 0; i < string.length; i++) {
+//         console.log(string[i], string[(string.length - 1) - i])
+//         if(string[i] !== string[(string.length - 1) - i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(isPalindrome('apple'))
