fc1cabaad5917558876938bb96e01e4ce6be7eb9
stringking <32914608+haptikfeedback@users.noreply.github.com>
2018-03-01 20:07:31 -0800

    added socks.py

---
+++ basic/socks.py
@@ -0,0 +1,43 @@
+import random
+
+sock_types = ['ankle', 'crew', 'calf', 'thigh']
+sock_colors = ['black', 'white', 'blue']
+socks = []
+colors = []
+colors_count = {}
+socks_count = {}
+
+def sockType():
+    for i in range(100):
+        socks.append(random.choice(sock_types))
+
+    for sock in socks:
+        if sock in socks_count.keys():
+            socks_count[sock] += 1
+        else: 
+            socks_count[sock] = 1
+
+    for sock, count in socks_count.items():
+        if count % 2 != 0:
+            print(f'{sock} has 1 loner')
+    # print(socks_count)
+
+def sockColor():
+    for i in range(100):
+        colors.append(random.choice(sock_colors))
+    
+
+    for color in colors:
+        if color not in socks_count.values():
+            socks_count[colors].append(color) 
+
+    print(color)
+    print(socks_count)
+    
+
+
+# def combineSocks():
+
+
+sockType()
+sockColor()
